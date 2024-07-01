02e9ea7cc8bcfdbd1d728e224093a51fbab23fbe
SAQUIB MANSURI <56463368+saquibmansuri@users.noreply.github.com>
2024-07-01 14:51:21 +0530

    Adds deployment of 2 functions in same azure function app

--- FunctionApp/DemoSample/Function2/__init__.py
+++ FunctionApp/DemoSample/Function2/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function - Function2 processed a request.')
 
     name = req.params.get('name')
     if not name:
@@ -16,9 +16,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function - Function2 executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. Hello I am Saquib",
+             "This HTTP triggered function - Function2 executed successfully. Pass a name in the query string or in the request body for a personalized response. Hello I am Saquib",
              status_code=200
         )
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # AzureFunction
-This repo contains a sample python function in FunctionApp/ directory and ci/cd pipeline to deploy it to azure functions app.
+This repo contains two sample python functions in FunctionApp/DemoSample directory and ci/cd pipeline to deploy both functions into 1 single azure function app.
 
 # If you want to make the function to be accessed by autherization then change this line in function.json
 "authLevel": "anonymous",  ------>>>  "authLevel": "Function",
 
 # Once the function is deployed then test it using hitting function URL with parameter ( I would suggest from postman)
-https://myfunction.azurewebsites.net/api/HttpTrigger1?name=saquib
-
+For Function1 -- https://myfunction.azurewebsites.net/api/Function1?name=saquib
+For Function2 -- https://myfunction.azurewebsites.net/api/Function2?name=saquib
